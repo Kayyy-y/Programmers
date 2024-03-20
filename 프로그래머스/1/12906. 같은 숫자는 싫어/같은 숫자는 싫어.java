9f703af2054a3eb8a12f6fabc364221aa52d7759
Lee JeongEon <62325770+Kayyy-y@users.noreply.github.com>
2024-03-21 01:54:27 +0900

    Update 같은 숫자는 싫어.java

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Solution {
+    // 마지막에 든 값이 뭔지 출력
     private static int getLast(List<Integer> lst){
         int lastidx = lst.size()-1;
         int ans = lst.get(lastidx);
@@ -8,15 +9,15 @@ public class Solution {
     }
     public int[] solution(int []arr) {
         List<Integer> answer = new ArrayList<>();
-        
+        //맨 앞 값은 무조건 들어감
         answer.add(arr[0]);
 
         for(int i = 1; i < arr.length; i++){
             if(arr[i] == getLast(answer)) continue;
             else answer.add(arr[i]);
-        }    
+        }
         int[] answer_arr = new int[answer.size()];
-            
+           
         for(int i = 0; i < answer_arr.length; i++){
             answer_arr[i] = answer.get(i);
         }
