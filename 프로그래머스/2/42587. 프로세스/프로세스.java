a717503e7f7455efcab46783f4f1b15f90094087
Lee JeongEon <62325770+Kayyy-y@users.noreply.github.com>
2024-03-24 03:31:11 +0900

    Update 프로세스.java

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,27 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
+
+      // 큐 사용!
         Queue<Integer> priQueue = new LinkedList<>();
         Queue<Integer> orderQueue = new LinkedList<>();
         List<Integer> order = new ArrayList<>();
-        
+
+      // order를 저장할 큐도 같이 생성
         for (int i = 0; i < priorities.length; i++) {
             priQueue.add(priorities[i]);
             orderQueue.add(i);
         }
 
+      // priQueue의 모든 값이 사라질 때까지 orderQueue를 생성
+      // 1번 규칙
         while (priQueue.peek() != null) {
             int value = priQueue.poll();
             int orderValue = orderQueue.poll();
-
+      // 2번 규칙
             if (priQueue.stream().filter(number -> number > value).count() == 0) {
                 order.add(orderValue);
+      // 3번 규칙
             } else {
                 priQueue.add(value);
                 orderQueue.add(orderValue);
             }
         }
-
+      // 순서는 +1 필요
         return order.indexOf(location) + 1;
     }
 }
