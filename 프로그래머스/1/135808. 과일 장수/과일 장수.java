4473ca8efb604d07bf1109b4375dcac57ce14a64
Lee JeongEon <62325770+Kayyy-y@users.noreply.github.com>
2024-03-24 03:00:57 +0900

    Update 과일 장수.java

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -5,7 +5,9 @@ class Solution {
         int answer = 0;
         
         Arrays.sort(score);
-        
+
+        // 뒤에서부터 잘라야 한 박스 안에 들어가는 사과값이 최대가 됨!
+        // 박스를 채우지 못하면 어차피 0원
         for(int i = score.length - m; i >= 0 ; i -= m) {
             answer += score[i] * m;
         }
