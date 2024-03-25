19f308e83ef5c57ed088f09849ee8fb34a58d671
Lee JeongEon <62325770+Kayyy-y@users.noreply.github.com>
2024-03-25 15:50:17 +0900

    Update 게임 맵 최단거리.java

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -20,33 +20,41 @@ class Solution {
         boolean[][] isVisited = new boolean[maps.length][maps[0].length];
         
         Queue<State> queue = new LinkedList<>();
+        // 초기 상태
         queue.add(new State(0, 0, 1));
         isVisited[0][0] = true;
         
         while(!queue.isEmpty()) {
             State state = queue.poll();
-            
+
+            // 상대 진영에 도착함
             if(state.y == maps.length - 1 && state.x == maps[state.y].length - 1) {
                 return state.step;
             }
-            
+
+            // 상하좌우로 이동
             for(int d = 0; d < 4; d++) {
                 int nx = state.x + dx[d];
                 int ny = state.y + dy[d];
-                
+
+                // 5x5를 넘는다면 실행x
                 if(ny < 0 || ny >= maps.length || nx < 0 || nx >= maps[ny].length) {
                     continue;
                 }
-                
+
+                // 벽이 있다면 실행x
                 if(maps[ny][nx] != 1) {
                     continue;
                 }
-                
+
+                // 방문한 적 있다면 실행x
                 if(isVisited[ny][nx]) {
                      continue;
                 }
-                
+
+                // 방문했다고 표시
                 isVisited[ny][nx] = true;
+                // 이동 후, 몇 번째인지 +1
                 queue.add(new State(nx, ny, state.step + 1));
             }
         }    
